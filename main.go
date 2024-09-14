5e437f129eebdb398dfaf846b4af1c1c1f841068
Abhinav Srivatsa <asrivatsa6@gmail.com>
2024-09-14 11:57:43 +0530

    14/09/24 run

--- main.go
+++ main.go
@@ -9,14 +9,23 @@ import (
 
 func saveArticles() news_scraper.Articles {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	s := news_scraper.NewMoneyControlScraper(logger)
+	s := news_scraper.NewEconomicTimesScraper(logger, "https://economictimes.indiatimes.com/lazy_list_tech.cms", map[string]string{
+		"information-tech": "78570530",
+		"technology":       "78570561",
+		"finance/banking":  "13358319",
+		"energy/power":     "13358361",
+	})
 
-	articles := s.Scrape(1, 30)
+	articles := s.Scrape(1, 10)
 
-	err := articles.Save("money-control-articles")
-	if err != nil {
-		panic(err)
-	}
+	// mcs := news_scraper.NewMoneyControlScraper(logger)
+
+	// articles := mcs.Scrape(1, 30)
+
+	// err := articles.Save("money-control-articles")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	return articles
 }
